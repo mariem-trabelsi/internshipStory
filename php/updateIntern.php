7bafefb451aeaee0d837ac6141a8b6501f81a151
Meriem <mariem.trabelsi@istic.ucar.tn>
2022-09-08 13:27:38 +0100

    css modifies logComponent

--- php/updateIntern.php
+++ php/updateIntern.php
@@ -3,8 +3,8 @@ header("Access-Control-Allow-Origin: *");
 require 'database.php';
 
 
-$stmt= $bdd->prepare('UPDATE INTERNS set NOM_INTERN =?,PRENOM_INTERN =?,REGION_INTERN =?,IMAGE_INTERN =?,INTERNMAIL =?,INTERNPSS =? where INTERNMAIL= ?');
-$stmt->execute(array($_GET['nom'],$_GET['prenom'],$_GET['region'],$_GET['image'],$_GET['email'],$_GET['psw'],$_GET['emailp']));
+$stmt= $bdd->prepare('UPDATE INTERNS set NOM_INTERN =?,PRENOM_INTERN =?,REGION_INTERN =?,IMAGE_INTERN =?,INTERNPSS =? where INTERNMAIL= ?');
+$stmt->execute(array($_GET['nom'],$_GET['prenom'],$_GET['region'],$_GET['image'],$_GET['psw'],$_GET['emailp']));
 $stmt->closeCursor();
 // }
 ?>
